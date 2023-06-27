5e85495bebb8fb9d73bbe3ad1b4df965baa5bf7c
areef44 <areef.empat.empat@gmail.com>
2023-06-27 22:09:23 +0700

    export notes implemented

--- src/server.js
+++ src/server.js
@@ -25,6 +25,11 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+// Exports
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const init = async () => {
   
   const collaborationsService = new CollaborationsService();
@@ -99,6 +104,13 @@ const init = async () => {
       validator: CollaborationsValidator,
       },
     
+  },
+  {
+    plugin: _exports,
+    options: {
+      service: ProducerService,
+      validator: ExportsValidator,
+    },
   },
 ]);
 

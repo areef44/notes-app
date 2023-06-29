2fa88c627a1a8f54e832fbb2d7cf74fbb5736b56
areef44 <areef.empat.empat@gmail.com>
2023-06-29 17:55:57 +0700

    server-side caching implemented

--- src/api/uploads/handler.js
+++ src/api/uploads/handler.js
@@ -15,11 +15,17 @@ class UploadsHandler {
 
             const filename = await this._service.writeFile(data, data.hapi);
 
+
+            // const fileLocation = await this._service.writeFile(data, data.hapi);
+
             const response = h.response({
                 status: 'success',
                 data: {
                     fileLocation: `http://${process.env.HOST}:${process.env.PORT}/upload/images/${filename}`,
                 },
+                // data: {
+                //     fileLocation,
+                // }
             });
             response.code(201);
             return response;
